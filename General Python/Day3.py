a59279c5bc8585d29aa40377ada24adcf06068be
BatteryOrVansh <officialvanshdixit@gmail.com>
2026-01-19 14:18:25 +0530

    Added day 3 Assignment Operators

---
+++ General Python/Day3.py
@@ -0,0 +1,57 @@
+# ASSIGNMENT OPERATOR
+
+# Q1. Basic Assignment
+# Assign the value 10 to a variable and print it. 
+x = 10
+print(x)
+
+
+# Q2. Add and Assign (+=)
+# Take a number, increase it by 5 using +=, and print the result. 
+y = 5
+y += 5
+print(y)
+
+
+# Q3. Subtract and Assign (-=)
+# Take a number, decrease it by 3 using -=, and print the result. 
+x -= 3
+print(x)
+
+
+# Q4. Multiply and Assign (*=)
+# Take a number, multiply it by 4 using *=, and print the result. 
+y *= 4
+print(y)
+
+
+# Q5. Divide and Assign (/=)
+# Take a number, divide it by 2 using /=, and print the result. 
+y /= 2
+print(y)
+
+
+# Q6. Floor Divide and Assign (//=)
+# Take a number, floor-divide it by 3 using //=, and print the result.
+z = 15
+z //= 3
+print(z)
+
+
+# Q7. Modulus and Assign (%=)
+# Take a number, find remainder when divided by 7 using %=, and print the result. 
+t = 49
+t %= 7
+print(t)
+
+
+# Q8. Exponent and Assign (**=)
+# Take a number, raise it to the power of 3 using **=, and print the result. 
+w = 5
+w**=3
+print(w)
+
+
+
+
+
