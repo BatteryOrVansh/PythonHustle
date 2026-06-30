051a0b708a0ad2a96513b00c0cacc1fdb6bc2d68
Vansh Dixit <officialvanshdixit@gmail.com>
2026-06-30 10:56:26 +0530

    add the functions for calculating exponents

--- Summer_Internship_Training/Day_5/functions.py
+++ Summer_Internship_Training/Day_5/functions.py
@@ -29,3 +29,18 @@ print("Multiplication of 10 and 20 is: ", multiplication)
 print("Division of 10 and 20 is: ", division)
 print("Floor Division of 10 and 20 is: ", floor_division)
 print("Remainder of 10 and 20 is: ", remainder)
+
+
+# exponent functions
+def square(x):
+    print(f"The square of {x} is {x**2}")
+    
+x = int(input("Enter a num for its square: "))
+square(x)
+
+
+def cube(n):
+    print(f"The cube of {n} is {n**3}")
+
+n = int(input("Enter a number for its cube: "))
+cube(n)
