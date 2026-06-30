040cb7518eeda0d37ff38d353fde7a5c5ba7be6c
Vansh Dixit <officialvanshdixit@gmail.com>
2026-06-30 10:20:27 +0530

    add factorial while loop

--- Summer_Internship_Training/LoopsPractice.py
+++ Summer_Internship_Training/LoopsPractice.py
@@ -3,3 +3,15 @@ for i in range(1, 50):
     if(i%3==0):
         print("Divisible by 3: ", i)
     
+# 2 Use a while loop to compute the factorial of a number entered by the user
+x = int(input("Enter a number to know its factorial: "))
+y = 1
+while(x>1):
+    y = y*x
+    x = x-1
+print("The factorial is : ", y)
+
+
+
+
+    
