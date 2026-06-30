871a68382ecb4fb3c7744e70dafa1c7ffd1ad896
Vansh Dixit <officialvanshdixit@gmail.com>
2026-06-30 10:32:52 +0530

    arithmetic operations as functions

--- Summer_Internship_Training/Day_5/functions.py
+++ Summer_Internship_Training/Day_5/functions.py
@@ -1,4 +1,31 @@
 def add(a,b):
     return a+b
-result = add(10,20)
-print(result)
+
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
+
+def floor_div(a,b):
+    return a//b
+
+def mod(a,b):
+    return a%b
+
+addition = add(10,20)
+subtraction = sub(10,20)
+multiplication = mul(10,20)
+division = div(10,20)
+floor_division = floor_div(10,20)
+remainder = mod(10,20)
+
+print("Addition of 10 and 20 is: ", addition)
+print("Subtraction of 10 and 20 is: ", subtraction)
+print("Multiplication of 10 and 20 is: ", multiplication)
+print("Division of 10 and 20 is: ", division)
+print("Floor Division of 10 and 20 is: ", floor_division)
+print("Remainder of 10 and 20 is: ", remainder)
